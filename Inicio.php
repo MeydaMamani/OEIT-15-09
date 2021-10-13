824c6ef9ad6eebaa232aa20e33140b0eeb528208
MeydaMamani <meydamamani@gmail.com>
2021-10-13 12:48:24 -0500

    imagen ibnicio

--- Inicio.php
+++ Inicio.php
@@ -6,15 +6,11 @@
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="./img/2.jpeg" class="d-block w-100" alt="..." w>
-        </div>
-        <div class="carousel-item">
-          <!-- <img src="./img/3.jpeg" class="d-block w-100" alt="..."> -->
         </div>
         <div class="carousel-item">
           <img src="./img/dires2.png" class="d-block w-100" alt="...">
